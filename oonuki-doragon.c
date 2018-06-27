5473e2a0576177f59e0ea94f410bef84ee155c27
ZEROssk <akinori_17N1101034@nnn.ed.jp>
2018-06-27 15:22:30 +0900

    ひとまず完成

--- oonuki-doragon.c
+++ oonuki-doragon.c
@@ -2,49 +2,58 @@
 #include <time.h>
 #include <stdlib.h>
 
-int St(int,char);
+int St(int, char *);
 
-int Player(int,char);
+int Player(int, char *);
 
 int Cr(int);
 
-int Ah(int,int);
+int Ah(int, int);
 
 int Sn(int);
 
-int En(void);
+int En(char *);
 
 int main(void)
 {
-    char name[1000];
+    char name[100];
     int EnemyHP = 100;
     int PlayerHP = 100;
     int ATK;
 
     printf("\nプレイヤー名を入力 = ");
-    scanf("%s\n", name);
-        printf("%s", name)
-    St(PlayerHP,name);
+    scanf("%s", name);
+    St(PlayerHP, name);
+    fflush(stdout);
     scanf("%d", &ATK);
     ATK = Cr(ATK);
-    EnemyHP = Ah(ATK,EnemyHP);
+    EnemyHP = Ah(ATK, EnemyHP);
     printf("ドラゴンに %d のダメージ\n", ATK);
 
     while (EnemyHP > 0)
     {
         Sn(EnemyHP);
-        Player(PlayerHP,name);
-        printf("\n%s攻撃\n攻撃力の設定 = ", name);
+        PlayerHP = Player(PlayerHP, name);
+        if (PlayerHP <= 0)
+        {
+            break;
+        }
+        printf("\n%sの攻撃\nEnemy HP = %d\n攻撃力の設定 = ", name, EnemyHP);
         scanf("%d", &ATK);
         ATK = Cr(ATK);
-        EnemyHP = Ah(ATK,EnemyHP);
-        printf("ドラゴンに %d のダメージ\n", ATK);
+        EnemyHP = Ah(ATK, EnemyHP);
+        printf("相手に %d のダメージ\n", ATK);
+        fflush(stdout);
     }
-    En();
+    if (PlayerHP <= 0) {
+        printf(" %sは死んでしまった\n", name);
+    } else {
+        En(name);
+    }
     return 0;
 }
 
-int St(int PlayerHP, char name) // 最初の表示
+int St(int PlayerHP, char *name) // 最初の表示
 {
     printf("\n┏━━━━━━━━━━━━━━━━━┓┏━━━━━━━━━━━━━━━┓\n");
     printf("┃ ドラゴンが現れた┃┃ Player1 HP %d┃\n", PlayerHP);
@@ -72,15 +81,18 @@ int Ah(int x, int h) // ダメージ計算
     return y;
 }
 
-int Player(int PlayerH, char name)   // プレイヤーに与えられるダメージ
+int Player(int PlayerH, char *name) // プレイヤーに与えられるダメージ
 {
     int kougeki;
     srand(time(NULL));
-    kougeki = rand() % 20 + 5;
-    printf("\n>相手の攻撃\n %s に %d のダメージ\n", name,kougeki);
+    kougeki = rand() % 20 + 15;
+    printf("\n>相手の攻撃\n %s に %d のダメージ\n", name, kougeki);
     PlayerH = PlayerH - kougeki;
-    printf(" %s HP = %d\n", name,PlayerH);
-    return 0;
+    if(PlayerH <= 0){
+        PlayerH = 0;
+    }
+    printf(" %s のHP = %d\n", name, PlayerH);
+    return PlayerH;
 }
 
 int Sn(int HP) // while文の中身 エネミー
@@ -89,10 +101,10 @@ int Sn(int HP) // while文の中身 エネミー
     return 0;
 }
 
-int En(void) // 最後の表示
+int En(char *name) // 最後の表示
 {
     int HP;
     printf("Enemy HP = 0\n");
-    printf("\n＿人人人人人人人人人＿\n＞ドラゴンを倒した！＜\n￣Y^Y^Y^Y^Y^Y^Y^Y^Y￣\n村人は1万Gを手に入れた\n");
+    printf("\n＿人人人人人人人人人＿\n＞ドラゴンを倒した！＜\n￣Y^Y^Y^Y^Y^Y^Y^Y^Y￣\n%sは1万Gを手に入れた\n", name);
     return 0;
 }
