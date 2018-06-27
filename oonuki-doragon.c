3885265d96929e8bdfb92d1058a7445ce933142e
ZEROssk <akinori_17N1101034@nnn.ed.jp>
2018-06-27 10:06:40 +0900

    1

---
+++ oonuki-doragon.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int St(int,char);
+
+int Player(int,char);
+
+int Cr(int);
+
+int Ah(int,int);
+
+int Sn(int);
+
+int En(void);
+
+int main(void)
+{
+    char name[1000];
+    int EnemyHP = 100;
+    int PlayerHP = 100;
+    int ATK;
+
+    printf("\nプレイヤー名を入力 = ");
+    scanf("%s\n", name);
+        printf("%s", name)
+    St(PlayerHP,name);
+    scanf("%d", &ATK);
+    ATK = Cr(ATK);
+    EnemyHP = Ah(ATK,EnemyHP);
+    printf("ドラゴンに %d のダメージ\n", ATK);
+
+    while (EnemyHP > 0)
+    {
+        Sn(EnemyHP);
+        Player(PlayerHP,name);
+        printf("\n%s攻撃\n攻撃力の設定 = ", name);
+        scanf("%d", &ATK);
+        ATK = Cr(ATK);
+        EnemyHP = Ah(ATK,EnemyHP);
+        printf("ドラゴンに %d のダメージ\n", ATK);
+    }
+    En();
+    return 0;
+}
+
+int St(int PlayerHP, char name) // 最初の表示
+{
+    printf("\n┏━━━━━━━━━━━━━━━━━┓┏━━━━━━━━━━━━━━━┓\n");
+    printf("┃ ドラゴンが現れた┃┃ Player1 HP %d┃\n", PlayerHP);
+    printf("┃ HP100           ┃┗━━━━━━━━━━━━━━━┛\n");
+    printf("┃ 討伐する▼       ┃\n");
+    printf("┗━━━━━━━━━━━━━━━━━┛\n");
+    printf("\n%sの攻撃\n攻撃力の設定 = ", name);
+    return 0;
+}
+
+int Cr(int atk) // クリティカル値
+{
+    int c;
+    srand(time(NULL));
+    c = rand() % 6;
+    atk = atk + c;
+    printf("クリティカル値 %d\n", c);
+    return atk;
+}
+
+int Ah(int x, int h) // ダメージ計算
+{
+    int y;
+    y = h - x;
+    return y;
+}
+
+int Player(int PlayerH, char name)   // プレイヤーに与えられるダメージ
+{
+    int kougeki;
+    srand(time(NULL));
+    kougeki = rand() % 20 + 5;
+    printf("\n>相手の攻撃\n %s に %d のダメージ\n", name,kougeki);
+    PlayerH = PlayerH - kougeki;
+    printf(" %s HP = %d\n", name,PlayerH);
+    return 0;
+}
+
+int Sn(int HP) // while文の中身 エネミー
+{
+    printf("Enemy HP = %d\n", HP);
+    return 0;
+}
+
+int En(void) // 最後の表示
+{
+    int HP;
+    printf("Enemy HP = 0\n");
+    printf("\n＿人人人人人人人人人＿\n＞ドラゴンを倒した！＜\n￣Y^Y^Y^Y^Y^Y^Y^Y^Y￣\n村人は1万Gを手に入れた\n");
+    return 0;
+}
